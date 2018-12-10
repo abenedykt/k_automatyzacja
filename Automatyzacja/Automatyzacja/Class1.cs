5308ef3a497a691e5753ddacebdf80106182a2f0
Jacek Kras (JKK) <JKK@kmd.dk>
2018-12-10 15:09:42 +0100

    testjkk-addedPostFlow

--- Automatyzacja/Automatyzacja/Class1.cs
+++ Automatyzacja/Automatyzacja/Class1.cs
@@ -38,7 +38,7 @@ namespace Automatyzacja
         [Fact]
         public void ExampleTest()
         {
-            browser.Navigate().GoToUrl("http://automatyzacja.benedykt.net/wp-admin");
+            browser.Navigate().GoToUrl("https://automatyzacja.benedykt.net/wp-admin");
             WaitForClickable(By.Id("user_login"),5);
             var userLogin = browser.FindElement(By.Id("user_login"));
 
@@ -54,6 +54,45 @@ namespace Automatyzacja
             var login = browser.FindElement(By.Id ( "wp-submit"));
             login.Click();
 
+            WaitForClickable(By.CssSelector(".wp-menu-name"),5);
+            var menuElements = browser.FindElements(By.CssSelector(".wp-menu-name"));
+            var posts = menuElements.Single(x => x.Text == "Wpisy");
+            posts.Click();
+
+            WaitForClickable(By.CssSelector(".wp-menu-name"), 5);
+            var submenuItems = browser.FindElements(By.CssSelector(".wp-submenu>li"));
+            var newPost = submenuItems.Single(x => x.Text == "Dodaj nowy");
+            newPost.Click();
+
+            WaitForClickable(By.Id("title"), 5);
+            var noteTitle = browser.FindElement(By.Id("title-prompt-text"));
+            noteTitle.Click();
+            var title = browser.FindElement(By.Id("title"));
+
+            title.SendKeys(Faker.Lorem.Sentence());
+
+            var html2Text = browser.FindElement(By.Id("content-html"));
+            html2Text.Click();
+
+            WaitForClickable(By.Id("publish"), 10);
+            WaitForClickable(By.CssSelector(".edit-slug.button"), 10);
+
+            var content = browser.FindElement(By.Id("content"));
+            content.SendKeys(Faker.Lorem.Paragraph());
+
+            WaitForClickable(By.CssSelector("input#publish"), 10);
+            var publish = browser.FindElement(By.CssSelector("input#publish"));
+            publish.Click();
+
+            //WaitForClickable(By.Id("publish"), 10);
+            //WaitForClickable(By.CssSelector(".edit-slug.button"), 10);
+
+            var posturl = browser.FindElement(By.CssSelector("#sample-permalink>a"));
+            var url = posturl.GetAttribute("href");
+
+
+
+            //var successmessage = browser.FindElements(By.ClassName(""));
         }
     }
 }
