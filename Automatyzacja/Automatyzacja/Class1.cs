ef4fea8f31f92891217b13119d91e9ed584fe663
Andrzej Witkowski <ajw@kmd.dk>
2018-12-10 11:33:23 +0100

    poprawa projektu

--- Automatyzacja/Automatyzacja/Class1.cs
+++ Automatyzacja/Automatyzacja/Class1.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
 using Xunit;
 
 namespace Automatyzacja
 {
-    public class Class1
+    public class Class1 : IDisposable
     {
+        private IWebDriver browser;
+
+        public Class1() => browser = new ChromeDriver();
+        public void Dispose() => browser.Quit();
+
         [Fact]
         public void ExampleTest()
         {
