8cb1292571bf13074410e1e35f204f306d8d300d
Andrzej Witkowski <ajw@kmd.dk>
2018-12-10 12:04:21 +0100

    2nd change

--- Automatyzacja/Automatyzacja/Class1.cs
+++ Automatyzacja/Automatyzacja/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using Xunit;
 
@@ -12,9 +13,27 @@ namespace Automatyzacja
         public Class1() => browser = new ChromeDriver();
         public void Dispose() => browser.Quit();
 
+        private void WaitForClickable(By by, int seconds)
+        {
+            var wait = new WebDriverWait(browser, TimeSpan.FromSeconds(seconds));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(by));
+        }
+        private void WaitForClickable(IWebElement element, int seconds)
+        {
+            var wait = new WebDriverWait(browser, TimeSpan.FromSeconds(seconds));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(element));
+        }
+
         [Fact]
         public void ExampleTest()
         {
+            browser.Navigate().GoToUrl("http://automatyzacja.benedykt.net/wp-admin");
+            WaitForClickable(By.Id("user_login"), 5);
+            browser.FindElement(By.Id("user_login")).SendKeys("automatyzacja");
+            WaitForClickable(By.Id("user_pass"), 5);
+            browser.FindElement(By.Id("user_pass")).SendKeys("jesien2018");
+            WaitForClickable(By.Id("wp-submit"), 5);
+            browser.FindElement(By.Id("wp-submit")).Click();
 
         }
     }
