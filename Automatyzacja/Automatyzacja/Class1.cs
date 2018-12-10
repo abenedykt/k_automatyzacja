d745783eabd1913e31e50813839e6f5e0a605a2d
Jacek Kras (JKK) <JKK@kmd.dk>
2018-12-10 12:04:54 +0100

    testjkk-WaitforClicable

--- Automatyzacja/Automatyzacja/Class1.cs
+++ Automatyzacja/Automatyzacja/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,11 +24,35 @@ namespace Automatyzacja
             browser.Quit();
         }
 
+        private void WaitForClickable(By by, int seconds)
+        {
+            var wait = new WebDriverWait(browser, TimeSpan.FromSeconds(seconds));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(by));
+        }
+        private void WaitForClickable(IWebElement element, int seconds)
+        {
+            var wait = new WebDriverWait(browser, TimeSpan.FromSeconds(seconds));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(element));
+        }
+
         [Fact]
         public void ExampleTest()
         {
             browser.Navigate().GoToUrl("http://automatyzacja.benedykt.net/wp-admin");
+            WaitForClickable(By.Id("user_login"),5);
+            var userLogin = browser.FindElement(By.Id("user_login"));
 
+ 
+            userLogin.SendKeys("automatyzacja");
+            WaitForClickable(By.Id("user_pass"), 5);
+            var userPass = browser.FindElement(By.Id("user_pass"));
+            userPass.SendKeys("jesien2018");
+            WaitForClickable(By.Id("rememberme"), 5);
+            var rememberme = browser.FindElement(By.Id("rememberme"));
+            rememberme.Click();
+            WaitForClickable(By.Id("wp-submit"), 5);
+            var login = browser.FindElement(By.Id ( "wp-submit"));
+            login.Click();
 
         }
     }
