8268290c14e8ee4b1c965a59e8485a8dbfe98439
Z6PMA <pma@kmd.dk>
2018-12-11 17:18:08 +0100

    added commenting test case

--- popDay2/NotePage.cs
+++ popDay2/NotePage.cs
@@ -4,19 +4,37 @@ using OpenQA.Selenium;
 
 namespace popDay2
 {
-	internal class NotePage
+	internal class NotePage : BasePage
 	{
-		private IWebDriver browser;
-		private Uri linkText;
+			private Uri linkText;
 
-		public NotePage(IWebDriver browser, Uri linkText)
+		public NotePage(IWebDriver browser, Uri linkText):base(browser)
 		{
-			this.browser = browser;
+	
 			this.linkText = linkText;
 			browser.Navigate().GoToUrl(linkText);
 		}
 		public string Title => browser.FindElement(By.ClassName("entry-title")).Text;
 		public string Content => browser.FindElement(By.ClassName("entry-content")).Text;
 
+		internal override bool IsAt()
+		{
+			return browser.FindElement(By.Id("comment")) != null;
+		}
+
+
+		internal void AddComment(string comment, string author, string email)
+		{
+			var newComment = browser.FindElement(By.Id("comment"));
+			newComment.SendKeys(comment);
+			
+			var newAuthor = browser.FindElement(By.Id("author"));
+			newAuthor.SendKeys(author);
+		
+			var newEmail = browser.FindElement(By.Id("email"));
+			newEmail.SendKeys(email);
+			var add = browser.FindElement(By.Id("submit"));
+			add.Click();
+		}
 	}
-	}
+}
---
+++ popDay2/commenting.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace popDay2
+{
+	public class Commenting : IDisposable
+	{
+		private IWebDriver browser;
+
+		
+		public  Commenting()
+		{
+			browser = new ChromeDriver();
+		}
+		[Fact]
+		public void canAddVisibleComment()
+		{
+			var comment = Faker.Lorem.Sentence();
+			var author = Faker.Lorem.Sentence();
+			var email = Faker.Internet.Email();
+			//go to site
+			var notePage = new NotePage(browser,new Uri( "http://automatyzacja.benedykt.net/uncategorized/sed-molestiae-quia-optio-est-voluptas-dolorum-reprehenderit"));
+			Assert.True(notePage.IsAt());
+			//add comment author email
+			notePage.AddComment(comment, author,email);
+			//check content
+			var commentList = browser.FindElement(By.Id("comments"));
+			var commentListTexts = commentList.Text;
+			Assert.Contains(comment, commentListTexts);
+			Assert.Contains(author, commentListTexts);
+		}
+
+		public void Dispose()
+		{
+			browser.Quit();
+		}
+	}
+}
