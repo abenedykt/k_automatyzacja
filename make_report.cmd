ae9ac2b7f345dc53a9b11725a4b86f1296bb169a
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2018-12-12 11:12:52 +0100

    added example cmd runner and report generator

---
+++ make_report.cmd
@@ -0,0 +1,2 @@
+Automatyzacja\packages\ReportUnit.1.2.1\tools\ReportUnit.exe testrun.xml report.html
+report.html
---
+++ run_tests.cmd
@@ -0,0 +1,1 @@
+ Automatyzacja\packages\xunit.runner.console.2.4.1\tools\net461\xunit.console.exe Automatyzacja\PageObjectExaple\bin\Debug\PageObjectExample.dll -xml testrun.xml
